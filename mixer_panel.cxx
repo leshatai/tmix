41af82557985c06b272b90b3bc53e7e93ce6f1d2
Martin Weise <contact@codefx.de>
2016-08-11 21:56:54 +0200

    added scale colors

--- mixer_panel.cxx
+++ mixer_panel.cxx
@@ -75,6 +75,10 @@ void MixerPanel::initPanel(){
         return;
     }
 
+	init_pair(COLOR_PAIR_GREEN, COLOR_GREEN, COLOR_BLACK);
+	init_pair(COLOR_PAIR_RED, COLOR_RED, COLOR_BLACK);
+	init_pair(COLOR_PAIR_WHITE, COLOR_WHITE, COLOR_BLACK);
+
     uint beginX = this->nr*10;
 
 	if (!this->mainWindow){
@@ -95,6 +99,31 @@ bool MixerPanel::isInitialized(){
     return (this->mainWindow && this->labelWindow && this->scaleWindow);
 }
 
+void MixerPanel::drawSingleScale(uint numLines, uint height, uint leftRight){
+    uint numWhite = numLines*0.2;
+    uint numGreen = numLines-(numWhite*2);
+    uint cp       = COLOR_PAIR_WHITE;
+
+    for(uint i=0; i < height; i++){
+        if(i > numWhite && i < numGreen){
+            cp = COLOR_PAIR_GREEN;
+        }
+        if(i > numGreen){
+            cp = COLOR_PAIR_RED;
+        }
+        wattron(this->scaleWindow.get(), COLOR_PAIR(cp));
+	    mvwprintw(this->scaleWindow.get(), numLines-i, leftRight, "%c", 177);
+        wattroff(this->scaleWindow.get(), COLOR_PAIR(cp));
+    }
+}
+
+void MixerPanel::clearScale(uint numLines){
+    for(uint i=0; i < numLines; i++){
+	    mvwprintw(this->scaleWindow.get(), numLines-i, 1, " ");
+	    mvwprintw(this->scaleWindow.get(), numLines-i, 2, " ");
+    }
+}
+
 void MixerPanel::drawScale(){
 	box(this->scaleWindow.get(), 0, 0);
     // as we draw a border, the first and the last line are already
@@ -103,20 +132,9 @@ void MixerPanel::drawScale(){
     uint heightLeft  = this->device.getVolumeLeft()*(numLines/100.0);
     uint heightRight = this->device.getVolumeRight()*(numLines/100.0);
 
-    // clear the scale
-    for(uint i=0; i < numLines; i++){
-	    mvwprintw(this->scaleWindow.get(), numLines-i, 1, " ");
-	    mvwprintw(this->scaleWindow.get(), numLines-i, 2, " ");
-    }
-
-    // draw left/right scale
-    for(uint i=0; i < heightLeft; i++){
-	    mvwprintw(this->scaleWindow.get(), numLines-i, 1, "%c", 177);
-    }
-
-    for(uint i=0; i < heightRight; i++){
-	    mvwprintw(this->scaleWindow.get(), numLines-i, 2, "%c", 177);
-    }
+    this->clearScale(numLines);
+    this->drawSingleScale(numLines, heightLeft, SCALE_LEFT);
+    this->drawSingleScale(numLines, heightRight, SCALE_RIGHT);
 
 	wrefresh(this->scaleWindow.get());
 }
--- mixer_panel.hpp
+++ mixer_panel.hpp
@@ -16,6 +16,13 @@ protected:
     static const uint HEIGHT_MAIN  = 30;
     static const uint HEIGHT_SCALE = 25;
 
+    static const uint COLOR_PAIR_GREEN  = 20;
+    static const uint COLOR_PAIR_RED    = 21;
+    static const uint COLOR_PAIR_WHITE  = 22;
+
+    static const uint SCALE_LEFT  = 1;
+    static const uint SCALE_RIGHT = 2;
+
 	uint nr;
 	MixerDevice &device;
 	std::shared_ptr<WINDOW> mainWindow;
@@ -26,6 +33,8 @@ protected:
     void initPanel();
     void drawLabel();
     void drawScale();
+    void drawSingleScale(uint numLines, uint height, uint leftRight);
+    void clearScale(uint numLines); 
 public:
 	MixerPanel(uint nr, MixerDevice &device);
 	~MixerPanel();
