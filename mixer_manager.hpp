3f7d7e5357b9ff45f9f0f31490c4d4d0198a3178
Martin Weise <contact@codefx.de>
2016-09-15 21:32:34 +0200

    added licence to all files

--- mixer_manager.hpp
+++ mixer_manager.hpp
@@ -1,3 +1,34 @@
+/*
+ * Abstract class to actually perform volume mixer settings.
+ * This way OS specific managers are separated from 'interface'
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * @(#) Copyright (c) 2016 Martin Weise
+ */
+
 #ifndef __MIXER_MANAGER_INTERFACE_H__
 #define __MIXER_MANAGER_INTERFACE_H__
 
@@ -11,7 +42,7 @@ class MixerManager {
         std::vector<MixerDevice> mixers;
         std::string device;
     public:
-        MixerManager(std::string device) : device(device) {}
+        MixerManager(std::string device) : device(std::move(device)) {}
 
         std::vector<MixerDevice> &getMixers() { return this->mixers; }
         uint getMixerCount() { return this->mixers.size(); }
